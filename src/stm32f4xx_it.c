ed7f80426e90f2e3e0c91242c5e39cd82767f0fa
Andy Best <Andy.Best@tribalworldwide.co.uk>
2015-02-20 16:47:18 +0000

    Added FreeRTOS

--- src/stm32f4xx_it.c
+++ src/stm32f4xx_it.c
@@ -116,14 +116,10 @@ void UsageFault_Handler(void)
   }
 }
 
-/**
-  * @brief  This function handles SVCall exception.
-  * @param  None
-  * @retval None
-  */
-void SVC_Handler(void)
+// Provided by FreeRTOS
+/*void SVC_Handler(void)
 {
-}
+}*/
 
 /**
   * @brief  This function handles Debug Monitor exception.
@@ -134,14 +130,11 @@ void DebugMon_Handler(void)
 {
 }
 
-/**
-  * @brief  This function handles PendSVC exception.
-  * @param  None
-  * @retval None
-  */
-void PendSV_Handler(void)
+// Provided by FreeRTOS
+/*void PendSV_Handler(void)
 {
 }
+*/
 
 /**
   * @brief  This function handles SysTick Handler.
