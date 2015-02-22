7535d0284e807d63309aafdb3898f34d576e8eff
Andy Best <andy.best@tribalddb.co.uk>
2015-02-22 15:41:51 +0000

    Increased serial baud rate to 115200

--- src/main.cc
+++ src/main.cc
@@ -95,7 +95,7 @@ void initUSB(void)
 void initUART(void)
 {
     UartHandle.Instance          = USARTx;
-    UartHandle.Init.BaudRate     = 9600;
+    UartHandle.Init.BaudRate     = 115200;
     UartHandle.Init.WordLength   = UART_WORDLENGTH_8B;
     UartHandle.Init.StopBits     = UART_STOPBITS_1;
     UartHandle.Init.Parity       = UART_PARITY_NONE;
