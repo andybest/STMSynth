70009d6e0795b6721cf7b9e9e03165715c32e653
Andy Best <andy.best@tribalddb.co.uk>
2015-02-22 15:22:38 +0000

    Task names working correctly

--- src/tasks/cpp_task.cc
+++ src/tasks/cpp_task.cc
@@ -6,11 +6,15 @@
 
 Task::Task(const char *name) : _taskHandle(NULL)
 {
+    _taskName = (char *)malloc(sizeof(strlen(name)) + 1);
     _taskName = strcpy(_taskName, name);
 }
 
 Task::~Task()
 {
+    if(_taskName != NULL){
+        free(_taskName);
+    }
 }
 
 void Task::start(osPriority priority)
