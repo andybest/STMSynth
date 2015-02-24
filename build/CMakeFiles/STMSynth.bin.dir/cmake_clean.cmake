6eb5dfc365ea3f88317ecbb8f5e56450b1ce8eb6
Andy Best <Andy.Best@tribalworldwide.co.uk>
2015-02-24 16:24:01 +0000

    Removed build directory

--- build/CMakeFiles/STMSynth.bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/STMSynth.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/STMSynth.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/program-flash.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/program-flash"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/program-flash.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
