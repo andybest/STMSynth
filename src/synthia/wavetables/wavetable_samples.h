95cf92d1d58a2985923b12b3139ef054d860dc82
Andy Best <Andy.Best@tribalworldwide.co.uk>
2015-02-24 16:24:28 +0000

    Added wavetables

---
+++ src/synthia/wavetables/wavetable_samples.h
@@ -0,0 +1,13 @@
+// Minimoog waveform samples (single cycle)
+
+extern int wavetable_pulse_len;
+extern float *wavetable_pulse;
+
+extern int wavetable_ramp_len;
+extern float *wavetable_ramp;
+
+extern int wavetable_sine_len;
+extern float *wavetable_sine;
+
+extern int wavetable_triangle_len;
+extern float *wavetable_triangle;
